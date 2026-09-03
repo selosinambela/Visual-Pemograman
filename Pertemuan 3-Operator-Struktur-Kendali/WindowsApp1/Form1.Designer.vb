54d21eecc1fe7820542b1858f40505c4eb4735f7
Vaidon Shello Sinambela <chelloaja47@gmail.com>
2026-09-03 23:28:50 +0700

    Upload Pertemuan 3 Operator Struktur Kendali

---
+++ Pertemuan 3-Operator-Struktur-Kendali/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInput = New System.Windows.Forms.Button()
+        Me.txtNilai = New System.Windows.Forms.TextBox()
+        Me.picImage = New System.Windows.Forms.PictureBox()
+        CType(Me.picImage, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnInput
+        '
+        Me.btnInput.Location = New System.Drawing.Point(328, 259)
+        Me.btnInput.Name = "btnInput"
+        Me.btnInput.Size = New System.Drawing.Size(75, 32)
+        Me.btnInput.TabIndex = 1
+        Me.btnInput.Text = "Input"
+        Me.btnInput.UseVisualStyleBackColor = True
+        '
+        'txtNilai
+        '
+        Me.txtNilai.Location = New System.Drawing.Point(305, 218)
+        Me.txtNilai.Name = "txtNilai"
+        Me.txtNilai.Size = New System.Drawing.Size(117, 26)
+        Me.txtNilai.TabIndex = 2
+        '
+        'picImage
+        '
+        Me.picImage.Location = New System.Drawing.Point(322, 91)
+        Me.picImage.Name = "picImage"
+        Me.picImage.Size = New System.Drawing.Size(100, 100)
+        Me.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picImage.TabIndex = 3
+        Me.picImage.TabStop = False
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.picImage)
+        Me.Controls.Add(Me.txtNilai)
+        Me.Controls.Add(Me.btnInput)
+        Me.Name = "form1"
+        Me.Text = "Form1"
+        CType(Me.picImage, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnInput As Button
+    Friend WithEvents txtNilai As TextBox
+    Friend WithEvents picImage As PictureBox
+End Class
---
+++ Pertemuan 3-Operator-Struktur-Kendali/WindowsApp1/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class form1
+    Private Sub txtNilai_TextChanged(sender As Object, e As EventArgs) Handles txtNilai.TextChanged
+
+    End Sub
+
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+        Dim nilaiUjian As Integer
+
+        If Not Integer.TryParse(txtNilai.Text, nilaiUjian) Then
+            MessageBox.Show("Masukkan dalam bentuk angka")
+            txtNilai.Focus()
+            Return
+        End If
+
+        If nilaiUjian < 0 OrElse nilaiUjian > 100 Then
+            MessageBox.Show("Masukkan nilai 0-100")
+            txtNilai.Focus()
+            Return
+        End If
+
+        If nilaiUjian <= 50 Then
+            picImage.Image = Image.FromFile("Assets\1.jpg")
+        ElseIf nilaiUjian <= 75 Then
+            picImage.Image = Image.FromFile("Assets\1.jpg")
+        Else
+            picImage.Image = Image.FromFile("Assets\1.jpg")
+        End If
+
+    End Sub
+
+    Private Sub txtNilai_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilai.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub picImage_Click(sender As Object, e As EventArgs) Handles picImage.Click
+
+    End Sub
+End Class
